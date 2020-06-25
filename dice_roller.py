26732b7042832300380286b98d44109d33715475
KK-ualberta <67343874+KK-ualberta@users.noreply.github.com>
2020-06-25 16:36:16 -0600

    tutorial complete for dice_roller.py
    added if else conditions for dice roll

--- dice_roller.py
+++ dice_roller.py
@@ -5,7 +5,12 @@ def main():
   for i in range(0, dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
-    print('You rolled a', roll)
+    if roll ==1 : 
+      print('You rolled a', roll, " gamer fail")
+    elif roll == 6:
+      print("You rolled a", roll, " gamer success")
+    else:
+      print("you rolled a", roll)
     print("you have rolled total of", dice_sum)
 if __name__== "__main__":
   main()
